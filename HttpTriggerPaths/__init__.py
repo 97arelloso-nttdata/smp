13d620b22ab7ba8504f3eb5322dd6ff40925d034
97arelloso-nttdata <111971921+97arelloso-nttdata@users.noreply.github.com>
2023-04-20 17:57:35 +0200

    Update __init__.py

--- HttpTriggerPaths/__init__.py
+++ HttpTriggerPaths/__init__.py
@@ -2,6 +2,8 @@ import logging
 
 import azure.functions as func
 from azure.storage.filedatalake import DataLakeServiceClient
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 import json
 
@@ -10,34 +12,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
 
     #connection_string = "DefaultEndpointsProtocol=https;AccountName=adlsmp;AccountKey=WNk38TUO/zv4natpUzAqoUfwEez1/a8zLc5r068VZWCCSqlKhQojpVWLtQeC/XT/RekMBMhxEOE1+ASt4L8KAw==;EndpointSuffix=core.windows.net"
-    storage_account_key = "WNk38TUO/zv4natpUzAqoUfwEez1/a8zLc5r068VZWCCSqlKhQojpVWLtQeC/XT/RekMBMhxEOE1+ASt4L8KAw=="
+    storage_account_key = "QXbwAa+LRPQCkSvYxpjM71po9Xiu8Bb4PWnJTi7NJNVqJgouukf6XqDctjvLchnJlFFiJ0/ayBECBPiaB1CUDQ=="
 
     # Get the env variable (Function App > Your_Function_App > Configuration > Under Application Settings)
     #KVUri = os.environ["KeyVaultUri"]
-    KVUri = "https://kv-smp.vault.azure.net/"
+    KVUri = "https://kv-dtlkrenewables-dev.vault.azure.net/"
 
     # Access Key Vault Secrets and specify what secret to access. In this case it's the storage account key
     # for later connection.
-    #credential = DefaultAzureCredential()
-    #client = SecretClient(vault_url=KVUri, credential=credential)
-    #storage_account_key = client.get_secret("storageAccountKey2").value
+    credential = DefaultAzureCredential(additionally_allowed_tenants=['*'])
+    client = SecretClient(vault_url=KVUri, credential=credential)
 
-    storage_account_name = "adlsmp"
-    contenedor = "fssmp"
+    #storage_account_key = client.get_secret("storageAccountKey2").value
+    print(client.get_secret("storageAccountKey").value)
+    storage_account_name = "dlsdatalakerenewablespro"
+    contenedor = "produccion"
 
     service_client_sink = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format("https", storage_account_name), credential=storage_account_key)
-
+ 
     # Generate the instances where files are stored in the ADL.
     # Define the ADL container.
     file_system_client = service_client_sink.get_file_system_client(file_system=contenedor)
-    directory_client_sink = file_system_client.get_directory_client("json")    
+    directory_client_sink = file_system_client.get_directory_client("smp/json")    
     file_client_sink = directory_client_sink.create_file("path.json")
 
     jsonInc = list()
 
     path_list = file_system_client.get_paths()
+
     for path in path_list:
-        if path.name.startswith("raw_data/") & path.name.endswith(".MED"):
+        if path.name.startswith("smp/raw_data/") & path.name.endswith(".MED"):
             file = path.name.split("/")[-1]
             windfarm = path.name.split("/")[-2]
             fullPath = path.name
